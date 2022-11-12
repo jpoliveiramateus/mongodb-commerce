d0dfff07462d6c67ccde65d63076121e4d2837f5
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-11-12 11:38:36 -0300

    refactor: desafio16

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,8 +1,8 @@
-// db.produtos.updateMany(
-//   { nome: "Big Mac" },
-//   { $currentDate: { ultimaModificacao: { $type: "timestamp" } } },
-// );
-// db.produtos.find(
-//   { ultimaModificacao: { $exists: true } },
-//   { _id: 0, nome: 1, ultimaModificacao: 1 },
-// );
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: true } },
+);
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 },
+);
