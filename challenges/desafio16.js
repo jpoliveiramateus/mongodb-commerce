35d6d5cd34f30e767964774c10f8e0d63078ef3f
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-11-12 05:44:25 -0300

    feat: bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo

---
+++ challenges/desafio16.js
@@ -0,0 +1,8 @@
+// db.produtos.updateMany(
+//   { nome: "Big Mac" },
+//   { $currentDate: { ultimaModificacao: { $type: "timestamp" } } },
+// );
+// db.produtos.find(
+//   { ultimaModificacao: { $exists: true } },
+//   { _id: 0, nome: 1, ultimaModificacao: 1 },
+// );
