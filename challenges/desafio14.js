f6e9bfc7082b60f3bc63e44c762dd0ecdc3602bf
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-11-12 11:25:16 -0300

    refactor: desafio14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,4 @@
-// db.produtos.find(
-//   { ingredientes: { $in: ["picles"] } },
-//   { _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $size: 3 } },
-// );
+db.produtos.find(
+  { ingredientes: { $in: ["picles"] } },
+  { _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } },
+);
